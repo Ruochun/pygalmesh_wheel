a8a3fc346c171668e3bd4b57019bc381604ce3c2
Ruochun <ruochunz@gmail.com>
2023-05-12 14:46:14 -0500

    Finish wheel generation

--- rot_geo_gen.py
+++ rot_geo_gen.py
@@ -51,92 +51,93 @@ def GenWheel(rad=0.25, width=0.2, cp_deviation=0., g_height=0.025, g_width=0.005
     wheel_peri = pygalmesh.Rotate(wheel_peri, [1, 0, 0], math.pi / 2)
 
     # Now add grousers
-    # 
-    g_seg = []
-    g_seed = np.array([0., -width/2, rad + g_height/2 - small_dist]) 
-    last_signed_bump = 0.
-    # Each grouser period, num of sample points
-    num_g_p_sample = 4 if not(g_curved) else 16
-    # Start and end t point of each period
-    s_e_t = np.linspace(0, 1, g_period+1)
-    for i in range(g_period):
-        start_t = s_e_t[i]
-        end_t = s_e_t[i+1]
-        per_seg_lin_length = width / g_period / num_g_p_sample
-        period_last_y = 0.
-        
-        for j in range(1, num_g_p_sample+1):
-            t = start_t + (end_t-start_t)/num_g_p_sample*j
-            b = pyBernstein(num_CPs - 1, t)
-            # eval_pnt[0] gives the vertical move dist
-            eval_pnt = b @ outer_CPs
-            signed_wheel_bump = eval_pnt[0] - rad
-            bump_diff = signed_wheel_bump - last_signed_bump
-            last_signed_bump = signed_wheel_bump
-
-            # planal length of this grouser segment
-            period_this_y = np.sin(math.pi*2/num_g_p_sample*j)*g_amp
-            planal_length = np.sqrt((period_this_y-period_last_y)**2 + per_seg_lin_length**2)
-            rot_angle = np.arctan((period_this_y-period_last_y)/per_seg_lin_length)
-            period_last_y = period_this_y
-
-            # finally, the cross-section parallelogram shape
-            parallelogram_deg = np.arctan(bump_diff/planal_length)
-            point2 = [planal_length+np.cos(parallelogram_deg)*wiggle_dist, -bump_diff-np.sin(parallelogram_deg)*wiggle_dist]
-            seg_length = np.sqrt(point2[0]**2 + point2[1]**2) - wiggle_dist # smaller... 
-            point3 = point2.copy()
-            point3[1] += g_height
-            parallelogram = pygalmesh.Polygon2D([[0,0], point2, point3, [0, g_height]])
-
-            # create the shape (bugged, only z direction works)
-            half1 = pygalmesh.Extrude(
-                parallelogram,
-                [0.0, 0.0, g_width/2],
-                0
-            )
-            half2 = pygalmesh.Extrude(
-                parallelogram,
-                [0.0, 0.0, -g_width/2],
-                0
-            )
-            seg = pygalmesh.Union([half1, half2])
-            # rotate so facing x
-            seg = pygalmesh.Translate(seg, [0, -g_height/2, 0])
-            seg = pygalmesh.Rotate(seg, [0,1,0], -math.pi/2)
-            seg = pygalmesh.Rotate(seg, [1,0,0], -math.pi/2)
+    # Loop through all grouser for a revolution of the wheel
+    for g_num in range(g_density):
+        # One grouser
+        g_seg = []
+        this_wheel_surf_rot = 0.
+        g_seed = np.array([0., -width/2, rad + g_height/2 - small_dist]) 
+        last_signed_bump = 0.
+        # Each grouser period, num of sample points
+        num_g_p_sample = 4 if not(g_curved) else 16
+        # Start and end t point of each period
+        s_e_t = np.linspace(0, 1, g_period+1)
+        for i in range(g_period):
+            start_t = s_e_t[i]
+            end_t = s_e_t[i+1]
+            per_seg_lin_length = width / g_period / num_g_p_sample
+            period_last_y = 0.
             
-            # rotate and move...
-            seg = pygalmesh.Rotate(seg, [0,0,1], rot_angle)
-            seg = pygalmesh.Translate(seg, g_seed.tolist())
-            g_seg.append(seg)
-            
-            # now where's the new g_seed?
-            g_seed += np.array(
-                [-seg_length*np.cos(parallelogram_deg)*np.sin(rot_angle),
-                seg_length*np.cos(parallelogram_deg)*np.cos(rot_angle),
-                seg_length*np.sin(parallelogram_deg)]
+            for j in range(1, num_g_p_sample+1):
+                t = start_t + (end_t-start_t)/num_g_p_sample*j
+                b = pyBernstein(num_CPs - 1, t)
+                # eval_pnt[0] gives the vertical move dist
+                eval_pnt = b @ outer_CPs
+                signed_wheel_bump = eval_pnt[0] - rad
+                bump_diff = signed_wheel_bump - last_signed_bump
+                last_signed_bump = signed_wheel_bump
+
+                # planal length of this grouser segment
+                period_this_y = np.sin(math.pi*2/num_g_p_sample*j)*g_amp
+                planal_length = np.sqrt((period_this_y-period_last_y)**2 + per_seg_lin_length**2)
+                rot_angle = np.arctan((period_this_y-period_last_y)/per_seg_lin_length)
+                period_last_y = period_this_y
+
+                # finally, the cross-section parallelogram shape
+                parallelogram_deg = np.arctan(bump_diff/planal_length)
+                point2 = [planal_length+np.cos(parallelogram_deg)*wiggle_dist, -bump_diff-np.sin(parallelogram_deg)*wiggle_dist]
+                seg_length = np.sqrt(point2[0]**2 + point2[1]**2) - wiggle_dist # smaller... 
+                point3 = point2.copy()
+                point3[1] += g_height
+                parallelogram = pygalmesh.Polygon2D([[0,0], point2, point3, [0, g_height]])
+
+                # create the shape (bugged, only z direction works)
+                half1 = pygalmesh.Extrude(
+                    parallelogram,
+                    [0.0, 0.0, g_width/2],
+                    0
                 )
+                half2 = pygalmesh.Extrude(
+                    parallelogram,
+                    [0.0, 0.0, -g_width/2],
+                    0
+                )
+                seg = pygalmesh.Union([half1, half2])
+                # rotate so facing x
+                seg = pygalmesh.Translate(seg, [0, -g_height/2, 0])
+                seg = pygalmesh.Rotate(seg, [0,1,0], -math.pi/2)
+                seg = pygalmesh.Rotate(seg, [1,0,0], -math.pi/2)
+                
+                # rotate and move...
+                seg = pygalmesh.Rotate(seg, [0,0,1], rot_angle)
+                seg = pygalmesh.Translate(seg, g_seed.tolist())
+                g_seg.append(seg)
+                
+                # now where's the new g_seed?
+                g_seed += np.array(
+                    [-seg_length*np.cos(parallelogram_deg)*np.sin(rot_angle),
+                    seg_length*np.cos(parallelogram_deg)*np.cos(rot_angle),
+                    seg_length*np.sin(parallelogram_deg)]
+                    )
 
-
-    # wheel = pygalmesh.Union(g_seg + [wheel_peri])
-
-    grousers = pygalmesh.Union(g_seg)
-    wheel = pygalmesh.Union([grousers, wheel_peri])
+        grouser_peri = pygalmesh.Union(g_seg)
+        wheel_peri = pygalmesh.Union([grouser_peri, wheel_peri])
+        # rotate this one grouser in-place
+        wheel_peri = pygalmesh.Rotate(wheel_peri, [0,1,0], g_angle)
+        # this_wheel_surf_rot += g_angle
 
     # if geo is disconnected, this function only creates mesh out of one piece of them...
     mesh = pygalmesh.generate_surface_mesh(
-        wheel,
+        wheel_peri,
         # bounding_sphere_radius = 1.0,
         min_facet_angle = 15.0,
-        max_radius_surface_delaunay_ball = 0.001,
-        max_facet_distance = 0.001,
+        max_radius_surface_delaunay_ball = 0.0025,
+        max_facet_distance = 0.0025,
         verbose = False
     )
-
-    # Create the grouser
 
     return mesh
 
 if __name__ == "__main__":
-    mesh = GenWheel(g_height=0.025, g_width=0.005, cp_deviation=-0.1, g_density=12, g_amp=0.03, g_period=3, g_curved=False)
+    mesh = GenWheel(g_height=0.025, g_width=0.005, cp_deviation=0.1, g_density=12, g_amp=0.03, g_period=3, g_curved=False)
     mesh.write("wheel.obj")
